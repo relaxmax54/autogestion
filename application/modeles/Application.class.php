de0b8ca370c00fc7f51bb7a9fe2b9af4b7b0cf00
relaxmax54 <maxime.code@bbox.fr>
2014-12-05 16:27:44 +0100

    ajout méthodes dans la classe Application

--- application/modeles/Application.class.php
+++ application/modeles/Application.class.php
@@ -1,5 +1,27 @@
 <?
 class Application extends Framework{
+	/**
+	* affiche les vues demandées par l'utilisateur
+	* @param $vue : nom du fichier requis 
+	*/
+	public function afficheVue($vue){
+		if(file_exists("application/vues/".$vue.".php")){
+			require("application/vues/".$vue.".php");
+		}else{
+			require("vues/".$vue.".php");
+		}
+	}
+	/**
+	* affiche les modules demandes par l'utilisateur
+	* @param $module : nom du fichier requis 
+	*/
+	public function afficheModule($module){
+		if(file_exists("application/modules/".$module.".php")){
+			require("application/modules/".$module.".php");
+		}else{
+			require("modules/".$module.".php");
+		}
+	}
 
 }
 ?>
