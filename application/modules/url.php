78548e3db0d110c7194a4e832af582ec56134ccf
relaxmax54 <maxime.code@bbox.fr>
2014-12-05 16:35:40 +0100

    +url.php

---
+++ application/modules/url.php
@@ -0,0 +1,13 @@
+<h4><b>MODULE URL</b></h4>
+<pre>
+<?
+print_r($_GET);
+ ?>
+</pre>
+<?
+//echo '<p>*'.$vue.'*</p>';
+echo '<p><a href="'.$this->root.'/comptes/">comptes</a></p>';
+echo '<p><a href="'.$this->root.'/synthese/">synthese</a></p>';
+echo '<p><a href="'.$this->root.'/bootstrap/">Bootstrap</a></p>';
+echo '<p><a href="'.$this->root.'/saisie/">saisie</a></p>';
+echo '<p><a href="'.$this->root.'/parametres/">personnalisation</a></p>';
